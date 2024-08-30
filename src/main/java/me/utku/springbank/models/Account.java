ac0d7cee3951911bd23b2ee4a3272266bcbef114
utkuonursahin <sahinutkuonur@gmail.com>
2024-08-30 14:56:11 +0300

    refactor: model refactor complete

--- src/main/java/me/utku/springbank/models/Account.java
+++ src/main/java/me/utku/springbank/models/Account.java
@@ -2,16 +2,25 @@ package me.utku.springbank.models;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.ManyToOne;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
+
+import java.math.BigDecimal;
 
 @Entity
 @Getter
 @Setter
+@Builder
 public class Account extends BaseEntity {
     @Column(unique = true)
-    private long accountId;
-    @ManyToOne
+    private long accountNumber;
+    @ManyToOne(fetch = FetchType.LAZY)
     private User owner;
+    private BigDecimal cash;
+
+    public Account() {
+    }
 }
