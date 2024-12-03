420f16c873e3e5e46deb4138a94c69c860c4b35e
utkuonursahin <sahinutkuonur@gmail.com>
2024-12-03 19:22:39 +0300

    feat: implement fixed deposit daily interest

--- src/main/java/me/utku/springbank/service/account/action/FixedDepositInterestAction.java
+++ src/main/java/me/utku/springbank/service/account/action/FixedDepositInterestAction.java
@@ -1,25 +1,27 @@
 package me.utku.springbank.service.account.action;
 
 import lombok.RequiredArgsConstructor;
+import me.utku.springbank.enums.account.AccountType;
 import me.utku.springbank.model.Account;
 import me.utku.springbank.repository.AccountRepository;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Service;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
 
 import java.math.BigDecimal;
 import java.util.List;
 
-@Service
+@Component
 @RequiredArgsConstructor
-public class InterestService implements ScheduledTask {
+public class FixedDepositInterestAction {
     private final AccountRepository accountRepository;
-
-    @Value("${spring.project.bank.interest_rate}")
+    private final static int ONE_DAY_TIMESTAMP = 1000 * 60 * 60 * 24;
+    @Value("${spring.project.bank.daily_interest_rate}")
     private float interestRate;
 
-    //@Scheduled(fixedDelay = 1000 * 60)
-    public void perform() {
-        List<Account> accounts = accountRepository.findAll();
+    @Scheduled(fixedDelay = ONE_DAY_TIMESTAMP)
+    public void execute() {
+        List<Account> accounts = accountRepository.findAllByAccountType(AccountType.FIXED_DEPOSIT);
         accounts.forEach(account -> {
             BigDecimal cash = account.getCash();
             account.setCash(cash.add(cash.multiply(BigDecimal.valueOf(interestRate))));
